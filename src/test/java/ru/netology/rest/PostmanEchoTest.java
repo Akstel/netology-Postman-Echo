6431fd7c5c30a937ab1e7e9ca14e6d4ab3b105ea
Aleksey Stepin <diver911@yandex.ru>
2022-01-14 09:57:45 +0300

    breaking the test again

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -13,13 +13,14 @@ class PostmanEchoTest {
         // Предусловия
         ValidatableResponse some_data = given()
                 .baseUri("https://postman-echo.com")
-                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .contentType("text/plain; charset=UTF-8")
+                .body("some data" + "Мы Вас приветствуем!") // отправляемые данные (заголовки и query можно выставлять аналогично)
                 // Выполняемые действия
                 .when()
                 .post("/post")
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body("some data", equalTo("data"));
+                .body("data", equalTo("some data"  + "Мы Вас приветствуем"));
     }
 }
